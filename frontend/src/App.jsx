78e0d8325aaa493d46a4dd42a4625dd5ac45c7d4
martinsegbe <martinsegbe17@gmail.com>
2023-12-13 16:41:50 +0100

    searchbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    // Implement your search logic here
+    console.log('Searching for:', searchTerm);
+    // You can perform additional actions based on the search term
+  };
 
   return (
     <>
@@ -25,11 +34,23 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <div className="search-container">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button className="btn btn-primary" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
