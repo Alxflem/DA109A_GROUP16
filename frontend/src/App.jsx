0febdc97efdd99428374a68d773aeef5c1cb218a
Alxflem <fleming.alexander99@gmail.com>
2024-01-06 15:45:51 +0100

    Re-haul of the UI.
    Removed all clutter.
    Cleaned up the code.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,52 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SearchContainer from './components/SearchContainer';
+import NavBar from './components/NavBar';
 
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const handleSearch = () => {
-    // Implement your search logic here
-    console.log('Searching for:', searchTerm);
-    // You can perform additional actions based on the search term
-  };
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <NavBar/>
+      <div className="outer-container">
+        <SearchContainer/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className="search-container">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Search..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button className="btn btn-primary" onClick={handleSearch}>
-          Search
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
---
+++ frontend/src/components/SearchContainer.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+function SearchContainer() { 
+  const handleSearch = () => {
+    // Implement your search logic here
+    console.log('Searching for:', searchTerm);
+    // You can perform additional actions based on the search term
+  };
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <div className='d-flex justify-content-center align-items-center'>
+      <div className="search-container">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <br />
+        <button className="btn btn-dark" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default SearchContainer;
